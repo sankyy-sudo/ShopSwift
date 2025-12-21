3e4326f4203776f212b81af67ec4ef8b24e2fb9d
sankyy-sudo <sanketmishra.dev@gmail.com>
2025-12-21 22:09:29 +0530

    new update

---
+++ src/main/java/com/ShopSwift/ShopSwift/ecommerce/exception/InvalidCredentialException.java
@@ -0,0 +1,7 @@
+package com.ShopSwift.ShopSwift.ecommerce.exception;
+
+public class InvalidCredentialException extends RuntimeException {
+    public InvalidCredentialException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/ShopSwift/ShopSwift/ecommerce/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ShopSwift.ShopSwift.ecommerce.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
